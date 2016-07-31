6298c4118b40821359a4dce623b9433d160e64a5
SKurosaki <st.4sns@gmail.com>
2016-08-01 01:43:57 +0900

    add menu list / add first

---
+++ WebApplication1/html/header.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8"></meta>
+    <link href="~/css/style.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet"></link>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
+
+    <title>ひよこ豆</title>
+</head>
+<body>
+     <header>
+        <div class="header_text">
+            <h1 id="title"> ひよこ豆</h1>
+            <h2 id="title"> http:// www.piyomame.com</h2>
+        </div>
+        <ul id="menu">
+            <li><a href="~/html/index.cshtml">TOP</a></li>
+            <li><a href="~/html/first.cshtml">FIRST</a></li>
+            <li><a href="~/html/img.cshtml">IMG</a></li>
+            <li><a href="~/html/memo.cshtml">MEMO</a></li>
+            <li><a href="~/html/sns.cshtml">SNS</a></li>
+        </ul>
+    </header>
+  </body>
+</html>      
