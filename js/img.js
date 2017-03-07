7f3744d5688d252f41caa62812f75a4d417cecf4
SKurosaki <st.4sns@gmail.com>
2017-03-07 21:51:06 +0900

    add img

--- js/img.js
+++ js/img.js
@@ -1,16 +1,236 @@
-// v$('.all').click(function(){
-//   $('.fan-art_img').show();
-// });
 
-// $('[class^="tab"]').click(function(c) {
-//   $('.fan-art_img').hide();
-//   $('.'+ this.className + "-i").show();
-//   $(this).off(c);
-// });
+$(function(addSearchWord){
+  // -- 1 : MA --
 
-// function tab12() {
-//   $('.fan-art_img').hide();
-//   $('.'+ this.className + "-i").show();
-//   $(this).off(c);
+  $('.antman').append('Ant Man Scott Lang アントマン スコットラング あんとまん すこっとらんぐ').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_11-i');
+  $('.bucky').append('James Buchanan Barnes Bucky Winter Soldier バッキーバーンズ ウィンターソルジャー ウィンソル ばっきーばーんず うぃんたーそるじゃー うぃんそる 冬兵士').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_2-i');
+  $('.cancer').append('Cancer キャンサー きゃんさー').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.cap').append('Captain America Steve Rogers キャプテンアメリカ スティーブロジャース きゃぷてんあめりか すてぃーぶろじゃーす キャップ').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_3-i');
+  $('.carina').append('Carina カリーナ かりーな').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+  $('.collector').append('Collector Taneleer Tivan コレクター タニリーアティバン これくたー たにりーあてぃばん').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.dark_elf').append('Dark Elf ダークエルフ だーくえるふ').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.deadpool').append('Deadpool デッドプール デップー でっどぷーる でっぷー 俺ちゃん').next().children('.fan-art_img').addClass('tab1-i tab1_2-i');
+  $('.destroyer').append('Destroyer デストロイヤー ですとろいやー').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.frigga').append('Frigga フリッガ ふりっが').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+  $('.fury').append('Nick Fury ニックフューリー にっくふゅーりー 長官').next().children('.fan-art_img').addClass('tab1-i tab1_3-i tab1_3_1-i');
+  $('.green_goblin').append('Green Goblin グリーンゴブリン ぐりーんごぶりん').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.groot').append('Groot グルート ぐるーと').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_15-i');
+  $('.hawk').append('Hawkeye Clint Barton ホークアイ クリントバートン ほーくあい くりんとばーとん 鷹さん').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_6-i');
+  $('.hit_mon').append('Hit Monkey ヒットモンキー ひっともんきー').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.hulk').append('Hulk Bruce Banner ハルク ブルースバナー はるく ぶるーすばなー 博士').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_4-i');
+  $('.ironfist').append('Iron Fist Daniel Danny Rand アイアンフィスト ダニーランド').next().children('.fan-art_img').addClass('tab1-i tab1_2-i');
+  $('.juggernaut').append('Juggernaut ジャガーノート じゃがーのーと').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.kurse').append('Algrim the Strong Kurse アルグリムザストロング カース あるぐりむざすとろんぐ かーす').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.loki').append('Loki Laufeyson ロキ ろき 邪神 로키').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_1_1-i');
+  $('.magneto').append('Magneto Erik Magnus Lehnsherr マグニート エリック レーンシャー　まぐにーと えりっく れーんしゃー').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_1_6-i');
+  $('.malekith').append('Malekith the Accursed マレキス まれきす').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_1_5-i');
+  $('.modok').append('MODOK M.O.D.O.K. モードック もーどっく').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.nat').append('Black Widow Natasha Romanoff ブラックウィドウ ナターシャロマノフ ぶらっくうぃどう なたーしゃろまのふ ロマ姐 姐さん').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_5-i');
+  $('.red_skull').append('Red Skull Johann Schmidt レッドスカル ヨハンシュミット レッスカさん れっどすかる よはんしゅみっと れっすかさん 레드 스컬').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_1_3-i');
+  $('.rocket').append('Rocket Raccoon ロケットラクーン ろけっとらくーん').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_16-i');
+  $('.shocker').append('Herman Schultz Shocker ハーマンシュルツ ショッカー はーまんしゅるつ しょっかー').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.spidey').append('Spider-Man Spider Man Peter Benjamin Parker Spidey スパイダーマン ピーターパーカー スパイディ すぱいだーまん ぴーたーぱーかー すぱいでぃ 親愛なる隣人').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_12-i');
+  $('.star_lord').append('Star-Lord Star Lord Peter Quill スターロード ピータークイル スタロ すたーろーど ぴーたーくいる すたろ').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_13-i');
+  $('.thor').append('Thor Odinson ソー そー 兄上 토르').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_1-i');
+  $('.tony').append('Iron Man IronMan  Tony Stark アイアンマン トニースターク あいあんまん とにーすたーく 社長').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_2-i');
+  $('.venom').append('Venom ヴェノム ヴぇのむ べのむ').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_1_7-i');
+  $('.wasp').append('Wasp ワスプ わすぷ').next().children('.fan-art_img').addClass('tab1-i tab1_2-i tab1_2_7-i');
+  $('.zemo').append('Baron Zemo バロンジモ ばろんじも').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+  $('.zola').append('Arnim Zola アーニムゾラ あーにむぞら').next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
 
-// }
+  //DWA
+  $('.akira').append('Akira Akatsuki アカツキアキラ あかつきあきら').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+  $('.chris_t').append('Chris Taylor クリステイラー くりすていらー').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+  $('.edward').append('Edward Grant エドワードグラント えどわーどぐらんと').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+  $('.hikaru').append('Hikaru Akatsuki アカツキヒカル あかつきひかる').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+  $('.jessica').append('Jessica Shannon ジェシカシャノン じぇしかしゃのん').next().children('.fan-art_img').addClass('tab1-i tab1_3-i');
+
+// ).next().children('.fan-art_img').addClass('tab1-i tab1_1-i');
+
+
+  //-- 2 : DI --
+  //BH6
+  $('.baymax').append('Baymax ベイマックス べいまっくす');
+  $('.fred').append('Fred フレッド フレディ ふれっど');
+  $('.gogo').append('Gogo Tomago ゴーゴートマゴ ゴーゴータマゴ ごーごーとまご ごーごーたまご ごご');
+  $('.hiro').append('Hiro Hamada ヒロハマダ ひろはまだ');
+  $('.honey').append('Honey Lemon ハニーレモン はにーれもん はにれも');
+  $('.tadashi').append('Tadashi Hamada タダシハマダ ただしはまだ');
+  $('.wasabi').append('Wasabi ワサビ わさび');
+  $('.yokai').append('Yokai ヨーカイ カブキマスクマン よーかい かぶきますくまん');
+  //LK
+  $('.banzai').append('Banzai バンザイ ばんざい');
+  $('.ed').append('Ed エド えど');
+  $('.hyenas').append('Hyenas ハイエナトリオ はいえなとりお');
+  $('.mufasa').append('Mufasa ムファサ むふぁさ').next().children('.fan-art_img').addClass('tab2-i tab2_1-i tab2_1_2-i');
+  $('.scar').append('Scar スカー すかー おじさん').next().children('.fan-art_img').addClass('tab2-i tab2_1-i tab2_1_1-i');
+  $('.shenzi').append('Shenzi シェンジ しぇんじ');
+  //NBC
+  $('.barrel').append('Barrel バレル ばれる');
+  $('.boogie').append('Oogie Boogie ウーギーブギー うーぎーぶぎー 親分').next().children('.fan-art_img').addClass('tab2-i tab2_2-i tab2_2_1-i');
+  $('.lock').append('Lock ロック ろっく');
+  $('.lsb').append('子鬼トリオ');
+  $('.jack').append('Jack Skellington ジャックスケリントン パンプキンキング じゃっくすけりんとん ぱんぷきんきんぐ');
+  $('.sally').append('Sally サリー さりー');
+  $('.shock').append('Shock ショック しょっく');
+  $('.zero').append('Zero ゼロ ぜろ');
+
+  //-- 3 : SW --
+  $('.ewok').append('Ewok イウォーク いうぉーく');
+  $('.leia').append('Leia Organa Princess Leia レイアオーガナ レイア姫 れいあおーがな れいあひめ 将軍');
+  $('.maul').append('Darth Maul ダースモール だーすもーる');
+  $('.ren').append('Kylo Ren カイロレン かいろれん');
+  $('.vader').append('*Darth Vader ダースベイダー だーすべいだー 米田');
+
+  //-- 5 : TD --
+  // SH
+  $('.sherlock').append('Sherlock Holmes シャーロックホームズ しゃーろっくほーむず').next().children('.fan-art_img').addClass('tab5-i tab5_1-i tab5_1_1-i');
+  $('.john').append('John Watson ジョンワトソン じょんわとそん').next().children('.fan-art_img').addClass('tab5-i tab5_1-i tab5_1_2-i');
+
+
+
+  // $('.').append('ロナン マジェスティ－로난 GOTG = 가디언즈 오브 갤럭시 or 가오갤').next().children('.fan-art_img').addClass('tab1-i tab1_1-i tab1_1_2-i');;
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+  // $('.').append('');
+
+  //-- SP --
+  $('.dwa').append('DWA');
+  $('.gb').append('神兄弟').next().children('.fan-art_img').addClass('tab6-i tab6_1-i');
+  $('.pt').append('ペットボトル');
+  $('.yb').append('弟組').next().children('.fan-art_img').addClass('tab6-i tab6_2-i');
+  $('.usa').append('うさぎ');
+
+  //++ ACTOR ++
+  // $('.akinnuoye_a').append('Adewale Akinnuoye Agbaje アドウェールアキノエアグバエ あどうぇーるあきのえあぐばえ');
+  // $('.benedict_c').append('Benedict Timothy Carlton Cumberbatch ベネディクト・カンバーバッチ べねでぃくとかんばーばっち');
+  // $('.benicio_d_t').append('Benicio Del Toro ベニチオデルトロ べにちおでるとろ');
+  // $('.bradley_c').append('Bradley Charles Cooper ブラッドリークーパー ぶらっとりーくーぱー');
+  // $('.chris_p').append('Chris Pratt Christopher Michael Pratt クリスプラット くりすぷらっと');
+  // $('.chris_e').append('Chris Evans クリスエヴァンス クリエヴァ くりすえヴぁんす');
+  // $('.chris_h').append('Chris Hemsworth クリスヘムズワース クリヘム くりすへむずわーす');
+  // $('.christopher_e').append('Christopher Eccleston クリストファーエクルストン くりすとふぁーえくるすとん');
+  // $('.hugo_w').append('Hugo Weaving ヒューゴウィーヴィング ひゅーごうぃーヴぃんぐ');
+  // $('.james_e_j').append('James Earl Jones ジェームズアールジョーンズ じぇーむずあーるじょーんず');
+  // $('.jeremy_i').append('Jeremy Irons ジェレミーアイアンズ じぇれみーあいあんず');
+  // $('.jeremy_r').append('Jeremy Renner ジェレミーレナー じぇれみーれなー');
+  // $('.ken_p').append('Ken Page ケンペイジ けんぺいじ');
+  // $('.mark_r').append('Mark Ruffalo マークラファロ まーくらふぁろ');
+  // $('.martin_f').append('マーティンフリーマン Martin John Christopher Freeman まーてぃんふりーまん');
+  // $('.ophelia_l').append('Ophelia Lovibond オフィリアラヴィボンド おふぃりあらヴぃぼんど');
+  // $('.paul_r').append('Paul Rudd ポールラッド ぽーるらっど');
+  // $('.rene_r').append('Rene Russo レネルッソ れねるっそ');
+  // $('.robert_d_j').append('Robert Downey Jr. RDJ ロバートダウニージュニア ろばーとだうにーじゅにあ');
+  // $('.samuel_l_j').append('Samuel L. Jackson サミュエルエルジャクソン さみゅえるえるじゃくそん');
+  // $('.scarlett_j').append('Scarlett Johansson スカーレットヨハンソン スカヨハ すかーれっとよはんそん');
+  // $('.sebastian_s').append('Sebastian Stan セバスチャンスタン セバスタ せばすちゃんすたん せばすた');
+  // $('.toby_j').append('Toby Jones トビージョーンズ とびーじょーんず');
+  // $('.tom_h').append('Tom Hiddleston Thomas William Hiddleston トムヒドルストン とむひどるすとん');
+  // $('.vin_d').append('Vin Diesel ヴィンディーゼル ヴぃんでぃーぜる');
+});
+
+
+  //search-box scroll
+  $(function(){
+    $(window).scroll(function(){
+      var obj_t_pos = $('#datatables_filter').offset().top;
+      var scr_count = $(document).scrollTop() + (window.innerHeight/2);
+      if(scr_count > obj_t_pos){
+        $('#datatables_filter').addClass('fixed');
+        $('#datatables_filter').removeClass('dataTables_filter');
+      }else{
+        $('#datatables_filter').removeClass();
+      }
+      var top = $(window).scrollTop();
+      if( top < 200) {
+        $('#datatables_filter').removeClass();
+        $('#datatables_filter').addClass('dataTables_filter');
+      }
+    });
+  });
+
+  //first tab
+  $(document).ready(function() {
+    $('.tab li:first').addClass('active');
+    $('.tab li').click(function() {
+      $('.tab li').removeClass('active');
+      $(this).addClass('active');
+      $('.area').hide();
+      $('.area_children').hide(); 
+      $($(this).find('a').attr('href')).fadeIn();
+      return false;
+    });
+  });
+
+  //second tab
+  $(document).ready(function() {
+    $('.tab_children li:first').addClass('active');
+    $('.tab_children li').click(function() {
+      $('.tab_children li').removeClass('active');
+      $(this).addClass('active');
+      $('.area_children').hide();
+      $($(this).find('a').attr('href')).fadeIn();
+      return false;
+    });
+  });
+
+  //third tab
+  $(document).ready(function() {
+    $('.tab_g_children li:first').addClass('active');
+    $('.tab_g_children li').click(function() {
+      $('.tab_g_children li').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
+
+  $('.all').click(function(){
+    $('.fan-art_img').show();
+  });
+
+  $('[class^="tab"]').click(function(c) {
+    var imgtab = '.'+ this.className + "-i"
+    $(".fan-art_img").css('display','none');
+    $(imgtab).show();
+    $('.paginate_button').click(function(){
+      $('.fan-art_img').hide();
+      $(imgtab).show();
+    });
+  });
+
+  $(function(){
+    $(".modalOpen").click(function(){
+      var navClass = $(this).attr("class"),
+      href = $(this).attr("href");
+        if(href === "#modal02") {
+          $(href).children(".inner").css("animation","modal 0.5s forwards");
+        }
+        $(href).fadeIn();
+        $(this).addClass("open");
+        return false;
+    });
+    $(".modalClose").click(function(){
+      var parentsID = $(this).parents(".modal").attr("id");
+      if(parentsID === "modal02") {
+        $(this).parents(".modal").children(".inner").css("animation","modalClose 0.5s forwards");
+      }
+      $(this).parents(".modal").fadeOut();
+      $(".modalOpen").removeClass("open");
+      return false;
+    });
+  });
+
+  $(function(){
+    $("#a_list dt").on("click", function() {
+      $(this).next().slideToggle();
+    });
+  });
